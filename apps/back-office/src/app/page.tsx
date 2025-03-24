3a289515c6fc3ea0a8b710e76d339eaef52dfc59
Muzammil Shabbir <muzammil.techliance.com>
2025-03-24 23:03:44 +0500

    finalize project structure

--- apps/back-office/src/app/page.tsx
+++ apps/back-office/src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button } from '@mcw/ui';
+import { Input } from '@mcw/ui';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-6">Welcome to Back Office</h1>
       <p className="text-xl mb-8">Admin/Therapist Dashboard</p>
-      <Button variant="primary" size="lg">Dashboard</Button>
+      <Button variant='secondary' size="lg">Dashboard</Button>
+      <Input />
     </main>
   );
 } 
